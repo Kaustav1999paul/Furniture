0c80260c0d8fe8add72e6e55e9253a05a2536c4d
Kaustav1999paul <kaustavpaul347@gamil.com>
2022-12-12 20:45:08 +0530

    Base

---
+++ app/src/main/java/com/example/furniture/Login.java
@@ -0,0 +1,26 @@
+package com.example.furniture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    TextView createAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        createAccount = findViewById(R.id.createAccount);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/furniture/Register.java
@@ -0,0 +1,35 @@
+package com.example.furniture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+
+    ImageView back;
+    TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        back = findViewById(R.id.back);
+        login = findViewById(R.id.login);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
